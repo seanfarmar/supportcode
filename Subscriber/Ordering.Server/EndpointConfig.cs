bbb5b312e04ac5904aa664ae09d37e4f91c6a59b
sfarmar <s.farmar@gmail.com>
2014-09-16 16:40:15 +0100

    Subscriber - now using (SQL) NHibernate for subscription storage persistence

--- Subscriber/Ordering.Server/EndpointConfig.cs
+++ Subscriber/Ordering.Server/EndpointConfig.cs
@@ -8,12 +8,29 @@ namespace Ordering.Server
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
 
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, IWantCustomInitialization
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher
+    {
+    }
+
+    public class Bootsrap : IWantCustomInitialization
     {
         public void Init()
         {
             Configure.Features.Disable<Sagas>();
             Configure.Features.Disable<TimeoutManager>();
+
+            // we want to use sql for persistance so we follow : http://docs.particular.net/nservicebus/relational-persistence-using-nhibernate---nservicebus-4.x
+            // i created a database called NSERVICEBUS manually
+
+            Configure.Instance
+                .UseNHibernateSubscriptionPersister(); // subscription storage using NHibernate
+
+
+
+            // .UseNHibernateTimeoutPersister() // Timeout Persistance using NHibernate
+            // .UseNHibernateSagaPersister(); // Saga Persistance using NHibernate
+            // In case you are using Gateway and would like to configure the persistence, also do the below:
+            //.UseNHibernateGatewayPersister(); // Gateway Persistance using NHibernate    
         }
     }
 }
