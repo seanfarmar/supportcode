a54e45eb5f1cb689bb77dc9fd7685980c51c180c
sfarmar <s.farmar@gmail.com>
2014-09-23 17:41:57 +0100

    Gateway

---
+++ Gateway/Headquarter/EndpointConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace Headquarter
+{
+    using NServiceBus;
+
+    // The endpoint is started with the RunGateway profile which turns it on. The Lite profile is also
+    // active which will configure the persistence to be InMemory
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
+    {
+        public void Init()
+        {
+            // Configure.Instance.RunGateway();
+
+            Configure.With()
+                .DefaultBuilder()
+                .FileShareDataBus(".\\databus");
+        }
+    }
+}
