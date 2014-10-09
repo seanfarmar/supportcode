5658a348394b9346de27a34f54854513445ddf9e
sfarmar <s.farmar@gmail.com>
2014-10-09 21:21:09 +0100

    Added Gateway1

---
+++ GateWay1/WebApplication1/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    using NServiceBus;
+    using NServiceBus.Installation.Environments;
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static IBus _bus;
+
+        private IStartableBus _startableBus;
+
+        public static IBus Bus
+        {
+            get { return _bus; }
+        }
+
+        protected void Application_Start()
+        {
+            Configure.ScaleOut(s => s.UseSingleBrokerQueue());
+
+            _startableBus = Configure.With()
+                .DefaultBuilder()
+                .UseTransport<Msmq>()
+                .UnicastBus()
+                .RunGateway() //this line configures the gateway
+                 .FileShareDataBus(".\\databus")
+                .CreateBus();
+
+            Configure.Instance.ForInstallationOn<Windows>().Install();
+
+            _bus = _startableBus.Start();
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_End()
+        {
+            _startableBus.Dispose();
+        }
+    }
+}
---
+++ GateWay1/WebApplication1/Handlers/PriceUpdatedHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Handlers
+{
+    using System;
+    using Headquarter.Messages;
+    using NServiceBus;
+
+    public class PriceUpdatedHandler : IHandleMessages<PriceUpdated>
+    {
+        public IBus Bus { get; set; }
+
+        public void Handle(PriceUpdated message)
+        {
+            Console.WriteLine("Price update for product: " + message.ProductId +  " received. Going to reply over channel: " + message.GetHeader(Headers.OriginatingSite));
+        }
+    }
+}
