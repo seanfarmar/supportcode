00d8a8304897a3d9c3e834fba83218c90a013bfd
sfarmar <s.farmar@gmail.com>
2014-12-02 07:49:56 +0000

    added 8021

---
+++ WebSqpHandlers 8021/Handlers/PlacementAddedToGripEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using NServiceBus;
+
+namespace WebSqpHandlers.Handlers
+{
+	public class Message
+	{
+
+	}
+
+    public class PlacementAddedToGripEventHandler : IHandleMessages<Message>
+    {
+        
+        public void Handle(Message message)
+        {
+			throw new Exception();
+        }
+    }
+}
