861e00388fb85d0b30b18cb14d2dd63ab2ee12d7
sfarmar <s.farmar@gmail.com>
2014-12-06 18:18:52 +0000

    7893 consuming messages of msmq

---
+++ 7893/Endpoint/UnderwritingResponseHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Endpoint
+{
+    using System;
+    using UnderwritingResponse;
+    using NServiceBus;
+
+    public class UnderwritingResponseHandler : IHandleMessages<UnderwritingResponse>
+    {
+        public void Handle(UnderwritingResponse message)
+        {
+            Console.WriteLine("handling UnderwritingResponse");
+        }
+    }
+}
