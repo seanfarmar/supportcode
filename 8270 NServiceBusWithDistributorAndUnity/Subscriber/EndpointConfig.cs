09d221af2210cd00bab4fbe461859ac69f036654
sfarmar <s.farmar@gmail.com>
2014-12-22 13:03:05 +0200

    Removing the Is distributor functionality

--- 8270 NServiceBusWithDistributorAndUnity/Subscriber/EndpointConfig.cs
+++ 8270 NServiceBusWithDistributorAndUnity/Subscriber/EndpointConfig.cs
@@ -1,10 +1,10 @@
 
 namespace Subscriber
 {
+    using System.Configuration;
     using NServiceBus;
     using NServiceBus.Installation.Environments;
     using NServiceBus.ObjectBuilder.Unity;
-    using System.Configuration;
     using System.Diagnostics;
     using NServiceBus.ObjectBuilder.Common.Config;
     
@@ -12,33 +12,46 @@ namespace Subscriber
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
-    [EndpointSLA("0:00:05")]
+    // [EndpointSLA("0:00:05")]
     public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
     {
-        public bool IsDistributor 
-        {
-            get
-            {
-                return bool.Parse(ConfigurationManager.AppSettings["IsDistributor"]);
-            }
-        }
-
         public void Init()
         {
             Configure.With()
-                .UnityBuilder()                 // does not work
+                .UnityBuilder() // does not work
                 //.DefaultBuilder()             // works
                 .UnicastBus()
-                .MsmqSubscriptionStorage()      //Default is RavenDb
-                .UseInMemoryTimeoutPersister()  //Default is RavenDb
-                ;
+                .MsmqSubscriptionStorage() //Default is RavenDb
+                .UseInMemoryTimeoutPersister();  //Default is RavenDb
 
-            if (IsDistributor)
-                Configure.With()
-                    .RunMSMQDistributor(withWorker: true);  // Distributor and worker
-            else
-                Configure.With()
-                    .EnlistWithMSMQDistributor();   // Worker
+            //if (IsDistributor)
+            //    Configure.With()
+            //        .RunMSMQDistributor(withWorker: true);  // Distributor and worker
+            //else
+            //    Configure.With()
+            //        .EnlistWithMSMQDistributor();   // Worker
         }
     }
+
+    //public class CustomeConfiguration : IWantToRunBeforeConfigurationIsFinalized
+    //{
+
+    //    public bool IsDistributor
+    //    {
+    //        get
+    //        {
+    //            return bool.Parse(ConfigurationManager.AppSettings["IsDistributor"]);
+    //        }
+    //    }
+
+    //    public void Run()
+    //    {
+    //        if (IsDistributor)
+    //            NServiceBus.Configure.With()
+    //                .RunMSMQDistributor(withWorker: true);  // Distributor and worker
+    //        else
+    //            NServiceBus.Configure.With()
+    //                .EnlistWithMSMQDistributor();
+    //    }
+    //}
 }
