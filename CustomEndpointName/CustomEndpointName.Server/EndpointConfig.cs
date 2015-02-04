7229f7fe73526cea7c4fb3fc32afd00c4f468ecf
sfarmar <s.farmar@gmail.com>
2015-02-04 13:06:54 +0000

    Aded CustomEndpointName test case 8776

---
+++ CustomEndpointName/CustomEndpointName.Server/EndpointConfig.cs
@@ -0,0 +1,20 @@
+
+namespace CustomEndpointName.Server
+{
+    using NServiceBus;
+
+	/*
+		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
+		can be found here: http://particular.net/articles/the-nservicebus-host
+	*/
+	public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
+    {
+	    public void Init()
+	    {
+            Configure.With()
+                .DefaultBuilder()
+                .UseInMemoryTimeoutPersister();
+                //.DefineEndpointName("ServiceForIntegration");
+	    }
+    }
+}
