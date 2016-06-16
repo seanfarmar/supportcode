2dcde3027ccdebf89df085c77ec0b8186865cb8a
sfarmar <s.farmar@gmail.com>
2016-06-16 12:25:00 +0100

    added custom ErrorHandling

---
+++ custom_errorhandling/Core_6/WithoutSLR/MyMessageHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region Handler
+public class MyMessageHandler : IHandleMessages<MyMessage>
+{
+    static ILog log = LogManager.GetLogger<MyMessageHandler>();
+    static ConcurrentDictionary<Guid, string> Last = new ConcurrentDictionary<Guid, string>();
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        log.Info($"ReplyToAddress: {context.ReplyToAddress} MessageId:{context.MessageId}");
+
+        string numOfRetries;
+        if (context.MessageHeaders.TryGetValue(Headers.Retries, out numOfRetries))
+        {
+            string value;
+            Last.TryGetValue(message.Id, out value);
+
+            if (numOfRetries != value)
+            {
+                log.Info($"This is second level retry number {numOfRetries}");
+                Last.AddOrUpdate(message.Id, numOfRetries, (key, oldValue) => numOfRetries);
+            }
+        }
+
+        throw new Exception("An exception occurred in the handler.");
+    }
+
+}
+#endregion
---
+++ custom_errorhandling/Core_6/WithoutSLR/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Features;
+using NServiceBus.Logging;
+
+static class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.ErrorHandling.WithoutSLR";
+        LogManager.Use<DefaultFactory>()
+            .Level(LogLevel.Warn);
+
+        #region DisableSLR
+        var endpointConfiguration = new EndpointConfiguration("Samples.ErrorHandling.WithoutSLR");
+        endpointConfiguration.DisableFeature<SecondLevelRetries>();
+        #endregion
+        endpointConfiguration.UseSerialization<JsonSerializer>();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        try
+        {
+            Console.WriteLine("Press enter to send a message that will throw an exception.");
+            Console.WriteLine("Press any key to exit");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    return;
+                }
+                var myMessage = new MyMessage
+                {
+                    Id = Guid.NewGuid()
+                };
+                await endpointInstance.SendLocal(myMessage)
+                    .ConfigureAwait(false);
+            }
+        }
+        finally
+        {
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
