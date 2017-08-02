e6a7c96612cb61cb117856c5bc5a66bab3976944
sfarmar <s.farmar@gmail.com>
2017-08-02 18:36:56 +0100

    added 00029570

---
+++ 00029570/Fbs.Hosts.Endpoints.ClientPayments/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Fbs.Hosts.Endpoints.ClientPayments
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    class Program
+    {
+        static void Main()
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "Fbs.Hosts.Endpoints.ClientPayments";
+            var endpointConfiguration = new EndpointConfiguration("Fbs.Hosts.Endpoints.ClientPayments");
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.EnableInstallers();
+
+            endpointConfiguration.UseTransport<SqlServerTransport>()
+                .ConnectionStringName("Test.Publisher.ClientPayments.Transport");
+            
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
